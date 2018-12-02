a83e2a03b8bba65ba0eff0e81c9506f92dbc5436
Paul <paulskirk53@gmail.com>
2018-12-02 14:10:53 +0000

    changes to ensure listening and writing are optimised

--- Radio_Shutter_v2/Radio_Shutter_v2.ino
+++ Radio_Shutter_v2/Radio_Shutter_v2.ino
@@ -1,7 +1,7 @@
-
+//change text to command_from_master to improve code undestanding
 // this routine receives commands from the radio master arduino - OS# CS# and SS#
 // data is only returned by SS# - if the shutter is open return char message 'open' or 'closed'
-// 
+//
 //
 
 #include <SoftwareSerial.h>
@@ -30,8 +30,8 @@ RF24 radio(7, 8); // CE, CSN
 #define Flapclosed     13
 
 
-const byte thisaddress[6] = "00002";            // 00002 the address of this arduino board/ transmitter
-const byte masterNodeaddress[6] = "00000";            // 00002 the address of this arduino board/ transmitter
+const byte thisaddress[6] =       "00002";            // 00002 the address of this arduino board/ transmitter
+const byte masterNodeaddress[6] = "00000";
 char message[10] = "";
 String receivedData;
 
@@ -50,6 +50,7 @@ void setup()
 
   radio.setPALevel(RF24_PA_MIN);
   radio.enableDynamicPayloads();
+  radio.setRetries(15, 15);
 
 
   radio.openReadingPipe(1, thisaddress);    // the 1st parameter can be any number 1 to 5 the master routine uses 1
@@ -80,8 +81,9 @@ void setup()
 
 void loop()
 {
-  radio.startListening();
-  delay(20);                    // just in case hardware needs time before next instuction exec
+  //  already done in setup radio.openReadingPipe(1, thisaddress);    // 00002
+ // radio.startListening();
+
   while (!radio.available())
   {
     //do nothing
@@ -91,34 +93,61 @@ void loop()
   if (radio.available())
   {
     char text[32] = "";             // used to store what the master node sent e.g AZ hash SA hash
-    shutter_status();               //sets 'message' to indicate shutter open/ closed
-    radio.writeAckPayload(1, &message, sizeof(message));   // populate the buffer with shutter status - this is the only thing ever returned
-    radio.read(&text, sizeof(text));                       // so is just a way of ensuring SS gets back in case the command received was SS
+    //shutter_status();
 
+    radio.read(&text, sizeof(text));
+
+
+    Serial.println("this is the shutter node  ");
     Serial.print("The text received from Mster was: ");
     Serial.println(text);
-    Serial.print("this is the shutter node  ");
+
 
     if (text[0] == 'C' && text[1] == 'S' && text[2] == '#') // close shutter command
     {
-      Serial.print ("received CS");
+      //Serial.print ("received CS");
       close_shutter();
 
     }
-    // populate for OS and CS
 
-    if (text[0] == 'O' && text[1] == 'S' && text[2] == '#') // close shutter command
+
+    if (text[0] == 'O' && text[1] == 'S' && text[2] == '#') // open shutter command
     {
-      Serial.print ("received OS");
+      //Serial.print ("received OS");
       open_shutter();
 
     }
-    if (text[0] == 'S' && text[1] == 'S' && text[2] == '#') // close shutter command
+    if (text[0] == 'S' && text[1] == 'S' && text[2] == '#') //  shutter status command
     {
-      Serial.print ("received SS");
+
       shutter_status();
 
-    }
+      Serial.println("the value of shutter status is ");
+      Serial.print(message);
+      Serial.println("--------------------------------------------------");
+
+
+      radio.openWritingPipe(masterNodeaddress);
+      radio.stopListening();
+
+      delay(50);   // to alow the master to change from tx to rx
+      bool rslt =       radio.write(&message, sizeof(message));
+      radio.startListening();          //straight away after write to master, in case anothe message is sent
+
+      if (rslt)
+    {
+      Serial.println("result of shutter Tx was true");
+      }
+      else
+      {
+        Serial.println("result of shutter Tx was error");
+      }
+
+      for ( int i = 0; i < 10; i++)                //initialise the message array back to nulls
+    {
+      message[i] = 0;
+      }                                           //end for
+    }                                             //endif SS
 
     text[0] = 0;   // set to null character
     text[1] = 0;
@@ -250,9 +279,9 @@ void shutter_status()
     message [5] = 'D';
     message [6] = '#';
     //Serial.println( "closed#");  // return closed to driver modded for radio
-    Serial.println("the value of shutter status is");
-    Serial.println(message);
-    Serial.println("--------------------------------------------------");
+    //Serial.println("the value of shutter status is");
+    //  Serial.println(message);
+    //    Serial.println("--------------------------------------------------");
 
   }
   else
@@ -263,13 +292,13 @@ void shutter_status()
     message [2] = 'E';
     message [3] = 'N';
     message [4] = '#';
-    Serial.println("the value of shutter status is");
-    Serial.println(message);
-    Serial.println("--------------------------------------------------");
+    message [5] = 0;
+    message [6] = 0;
+    //Serial.println("the value of shutter status is");
+    //  Serial.println(message);
+    //    Serial.println("--------------------------------------------------");
   }
-  radio.stopListening();
-  radio.openWritingPipe(masterNodeaddress);
-  radio.write(&message, sizeof(message));
+
 
 }
 
