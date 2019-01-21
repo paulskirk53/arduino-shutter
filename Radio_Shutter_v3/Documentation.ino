736535ac91618107e22ad65bf7b1b11a3adc8666
Paul <paulskirk53@gmail.com>
2019-01-21 17:39:43 +0000

    rubbish commit - ignore

--- Radio_Shutter_v3/Documentation.ino
+++
@@ -1,8 +0,0 @@
-/*December 2018 
-Changes to limit switches coded in this version
-The one switch is now operated from the periphery of the wheel and the code counts the number of revolutions to open state/ closed state
-The flap is unaffected.
-November 2018
-It is only possible to transmit a char array[32] max size using radio.write. No other data format works
-*/
-
