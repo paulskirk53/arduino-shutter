084fcf9d7ffcae266d39ce9dcca760749ec86279
Paul <paulskirk53@gmail.com>
2019-11-02 23:06:04 +0000

    tidy up and consistent approach
    Set values of variables in the setup routine rather than in the declarations section. Tidy and remove some old comments.

--- Radio_Shutter_v3/Radio_Shutter_v3.ino
+++ Radio_Shutter_v3/Radio_Shutter_v3.ino
@@ -1,14 +1,16 @@
-// need to decide what last state is set to 
 
 // NB modded to only run the stepper for open and close - remove the commented out lines for resin flap open and close for a fully functioning version.
-// if emergency stop is pressed, to continue operation, the following procedure is required:
-// if emergency stop interrupts an open operation, the system thinks it is open, so press the close button
-// if emergency stop interrupts a close operation, the system thinks it is closed, so press the open button
+
+// if emergency stop is pressed, in order to continue operation, the following procedure is required:
+// if emergency stop interrupts an open operation, the system sets the last_state variable to open, so press the close button to move from the partially
+// open state
+// if emergency stop interrupts a close operation, the system sets the last_state variable to closed, so press the open button to move from the partially
+// closed state
 
 
 
 //this is the BOLLOCKS to BREXIT version October 2019
-//It will have a stepper control for the shutter belt drive and a DC motor control section for the actuator which opens the bottom door
+//It has a stepper control for the shutter belt drive and a DC motor control section for the actuator which opens the bottom flap
 //
 // this routine processes commands handed off by the command processor. This means that processes executed here (open close shutter and flap)
 // are non blocking from the ASCOM driver's perspective.
@@ -18,6 +20,9 @@
 
 //New November 2019 Shutter open and close by physical button press.
 // investigate just adding temp action buttons onto the open and close pins - 36 and 47 (open, close)
+
+
+// Compiler declarations follow
 
 #include <AccelStepper.h>
 
@@ -38,19 +43,19 @@ AccelStepper  stepper(AccelStepper::DRIVER, stepPin, dirPin, true);
 // shutter microswitch pin definitions
 
 //Pin 11 was defined here previously, but no longer used. It is still brought out from the arduino as pin 11 blue wire to the terminal block
-#define Flapopen              42             // connected to new limit mechanism with aluminium and wood disc actuator
-#define Flapclosed            38             // connected to new limit mechanism with aluminium and wood disc actuator
+#define Flapopen              42             // these two pins are connected to new limit mechanism for the lower flap
+#define Flapclosed            38             
 
 #define open_shutter_command  36             // input pin
 #define close_shutter_command 47             // input pin
 #define emergency_stop	      30             // input pin
 #define shutter_status        48             // OUTPUT pin
 
-String last_state        = "closed";
-long openposition =   1000;                   // change this to the number of steps required to move the shutter to its open position
-long closeposition =  0;                     //
-int normalAcceleration;
-float StepsPerSecond;
+String last_state         ;
+long   openposition       ;
+long   closeposition      ; 
+int    normalAcceleration ;
+float  StepsPerSecond     ;
 
 
 // end declarations -------------------------------------------------------------------------------------------------------
@@ -58,48 +63,53 @@ float StepsPerSecond;
 void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
 
-	Serial.begin(9600);                      //not required outside of testing
+	Serial.begin(9600);                               // not required outside of testing
 
 	// Define the pin modes. This avoids pins being low (which activates relays) on power reset.
-	// pinmodes for the open and close command pins and the shutter status pin
-	pinMode(open_shutter_command,  INPUT_PULLUP);
-	pinMode(close_shutter_command, INPUT_PULLUP);
-	pinMode(emergency_stop,        INPUT_PULLUP);     // user push button to stop the motor
-	pinMode(shutter_status,        OUTPUT);           //this routine sets this pin and it is read by the command processor arduino
+	// pinmodes for the open, close and emergency stop command pins and the shutter status pin
+	pinMode(open_shutter_command,  INPUT_PULLUP) ;
+	pinMode(close_shutter_command, INPUT_PULLUP) ;
+	pinMode(emergency_stop,        INPUT_PULLUP) ;    // user push button to stop the motor
+	pinMode(shutter_status,        OUTPUT)       ;    // this routine sets this pin and it is read by the command processor arduino
 
-	digitalWrite(shutter_status,   HIGH);            // HIGH means closed
+	digitalWrite(shutter_status,   HIGH);             // HIGH means closed
 
 	// pinmodes for resin flap relays
 	// Initialise the Arduino data pins for OUTPUT
 
-	pinMode(FLAPRELAY1,            OUTPUT);
-	pinMode(FLAPRELAY2,            OUTPUT);
+	pinMode(FLAPRELAY1,            OUTPUT) ;
+	pinMode(FLAPRELAY2,            OUTPUT) ;
 
 	
 	// initialise the pins for shutter and flap microswitches - input_pullup sets initial state to 1
 
-	pinMode (Flapopen,             INPUT_PULLUP);
-	pinMode (Flapclosed,           INPUT_PULLUP);
+	pinMode (Flapopen,             INPUT_PULLUP) ;
+	pinMode (Flapclosed,           INPUT_PULLUP) ;
 
 
 	// ALL THE RELAYS ARE ACTIVE LOW, SO SET THEM ALL HIGH AS THE INITIAL STATE
 
-	initialise_relays();      // sets all the relay pins HIGH for power off state
+	initialise_relays();                            // sets all the relay pins HIGH for power off state
 
 	//stepper setup:
-	StepsPerSecond = 1000.0;                       // changed following empirical testing
-	normalAcceleration = 50;                       // changed following empirical testing
-	stepper.setMaxSpeed(StepsPerSecond);          // steps per second see below -
+	StepsPerSecond     = 1000.0 ;                  // changed following empirical testing
+	normalAcceleration = 50     ;                  // changed following empirical testing
+	stepper.setMaxSpeed(StepsPerSecond);           // steps per second see below -
 	// the controller electronics is set to 0.25 degree steps, so 15 stepspersecond*0.25= 3.75 degrees of shaft movement per second
 	stepper.setAcceleration(normalAcceleration);
-	stepper.setCurrentPosition(closeposition);   //intial position for stepper is closed
+	stepper.setCurrentPosition(closeposition);     // intial position for stepper is closed
 	
+
+	last_state    = "closed" ;
+	openposition  = 1000     ;                     // change when implemented
+	closeposition = 0        ;
+
 	// delay below introduced to give the command processor time to define its pin states, which are used by this sketch
 	//there was a problem where relay 1 (OS) was activated due to indeterminate state of pin 46 as was the thinking.
 	//this delay seemed to cure the problem. fURTHER ANALYSIS - 30-3-19 shows that if power is lost to the two arduino boards
 	// i.e. command processor and shutter, the relay system activates relay 1 which is open flap
 
-	delay(5000);
+	delay(5000) ;
 	Serial.println("Shutter Processor ready");
 
 
@@ -113,9 +123,9 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 	{
 		Serial.println("received open");               // testing only print this to sermon when 36 was grounded
 		
-		open_process();
+		open_process() ;
 		digitalWrite(shutter_status, LOW) ;            // set the status pin - low is shutters open
-		last_state = "open";
+		last_state = "open" ;
 
 	}
 
@@ -135,8 +145,8 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 void initialise_relays() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
 	//  Serial.println( "  Initialising relays ");
-	digitalWrite(FLAPRELAY1,    HIGH);
-	digitalWrite(FLAPRELAY2,    HIGH);
+	digitalWrite(FLAPRELAY1,    HIGH) ;
+	digitalWrite(FLAPRELAY2,    HIGH) ;
 
 } // end intialise relays ---------------------------------------------------------------------------------------------------
 
