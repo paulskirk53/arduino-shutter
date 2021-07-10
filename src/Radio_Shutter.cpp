4b6d078824ddf04385846fa601589dacdebc3165
Paul <paulskirk53@gmail.com>
2021-07-10 13:03:22 +0100

    Updates to the code for use with Pulsar Dome

--- src/Radio_Shutter.cpp
+++ src/Radio_Shutter.cpp
@@ -1,4 +1,8 @@
-//Version 1.0 update the variable pkversion too 
+//July 9th started removing the flap based code as this is not required for the pulsar dome
+// this file was a clone done on 9th July from origin so is a working version for the new Pulsar dome
+
+
+
 //If open or close is interrupted by an emergency stop button press, just press the same button again e.g. Open/ ES/ Open
 //It has a stepper control for the shutter belt drive and a DC motor control section for the actuator which opens the bottom flap
 //
@@ -9,10 +13,10 @@
 // data is only returned by SS# - if the shutter is open return char message 'open' or 'closed'
 
 
-
+#include "Radio_shutter.h"
 // Compiler declarations follow
 
-#include <AccelStepper.h>
+
 
 // step, dir and enable pin definitions
 #define stepPin               7             // step pin tested and works - motor moves
@@ -22,24 +26,15 @@
 // create the stepper instance
 AccelStepper  stepper(AccelStepper::DRIVER, stepPin, dirPin, true);
 
-// shutter relay pin definitions
-
-// These data pins link to  Relay board pins IN1, IN2, in3 and IN4
-#define FLAPRELAY1             4             // arduino  pin 4
-#define FLAPRELAY2             5             // arduino  pin 5
-
-// shutter microswitch pin definitions
 
 //Pin 11 was defined here previously, but no longer used. It is still brought out from the arduino as pin 11 blue wire to the terminal block
-#define Flapopen                  42             // these two pins are connected to new limit mechanism for the lower flap
-#define Flapclosed                38
 
 #define open_shutter_command      36             // input pin
 #define close_shutter_command     47             // input pin
 #define emergency_stop	          30             // input pin
 #define shutter_status            48             // OUTPUT pin
-#define push_button_open_shutter  52             // green to hand controll switch unit
-#define push_button_close_shutter 53             // blue to hand controll switch unit
+#define push_button_open_shutter  52             // green to hand control switch unit
+#define push_button_close_shutter 53             // blue to hand control switch unit
 
 String last_state           ;
 long   openposition         ;
@@ -56,7 +51,7 @@ bool   close_command        ;
 void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
 
-  Serial.begin(19200);                               // not required outside of testing
+  //Serial.begin(19200);                               // not required outside of testing
 
   // Define the pin modes. This avoids pins being low (which activates relays) on power reset.
   // pinmodes for the open, close and emergency stop command pins and the shutter status pin
@@ -70,23 +65,7 @@ void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   digitalWrite(shutter_status,        HIGH);             // HIGH means closed
 
-  // pinmodes for flap relays
-  // Initialise the Arduino data pins for OUTPUT
-
-  pinMode(FLAPRELAY1,            OUTPUT) ;
-  pinMode(FLAPRELAY2,            OUTPUT) ;
-
-
-  // initialise the pins for shutter and flap microswitches - input_pullup sets initial state to 1
-
-  pinMode (Flapopen,             INPUT_PULLUP) ;
-  pinMode (Flapclosed,           INPUT_PULLUP) ;
-
-
-  // ALL THE RELAYS ARE ACTIVE LOW, SO SET THEM ALL HIGH AS THE INITIAL STATE
-
-  initialise_relays();                            // sets all the relay pins HIGH for power off state
-
+  
   //stepper setup:
   StepsPerSecond     = 500.0 ;                   // changed following empirical testing
   normalAcceleration = 50     ;                  // changed following empirical testing
@@ -106,7 +85,7 @@ void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   //there was a problem where relay 1 (OS) was activated due to indeterminate state of pin 46 as was the thinking.
 
   delay(5000) ;
-  Serial.println("Shutter Processor ready");
+  //Serial.println("Shutter Processor ready");
 
 
 }  // end setup -----------------------------------------------------------------------------------------------------------
@@ -125,7 +104,7 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
     //Serial.println(digitalRead(close_shutter_command));
     //delay(500);
     //
-// if either the ascom driver or the manual pushbotton asserts open
+// if either the ascom driver or the manual pushbutton asserts open
     if ( (digitalRead(open_shutter_command) == LOW)  | (digitalRead(push_button_open_shutter) == LOW)) //
     {
       open_command  = true;
@@ -146,7 +125,7 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     if (open_command && (last_state == "closed"))    // open shutter command
     {
-      Serial.println("received open");               // testing only print this to sermon when 36 was grounded
+      // Serial.println("received open");               // testing only print this to sermon when 36 was grounded
 
       open_shutter() ;
       digitalWrite(shutter_status, LOW) ;            // set the status pin - low is shutters open
@@ -155,7 +134,7 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
     if (close_command && (last_state == "open")) // close shutter command
     {
-      Serial.println("received close");          // testing only
+      // Serial.println("received close");          // testing only
       
       close_shutter();
       digitalWrite(shutter_status, HIGH) ;       // set the status pin - high is closed
@@ -170,14 +149,14 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 void open_shutter()  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
-  Serial.println("open shutter process");  // testing only
+  //Serial.println("open shutter process");  // testing only
 
 
   stepper.moveTo(openposition);
 
 
-  Serial.print("distance to go function value " );
-  Serial.println(stepper.distanceToGo());
+  //Serial.print("distance to go function value " );
+  //Serial.println(stepper.distanceToGo());
 
 
   while ( (stepper.distanceToGo() != 0) && (digitalRead(emergency_stop) == LOW) )
@@ -197,21 +176,21 @@ void open_shutter()  // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   }
 
 
-  Serial.print("Steper current position is ");
-  Serial.println(stepper.currentPosition());
+  //Serial.print("Steper current position is ");
+  //Serial.println(stepper.currentPosition());
 
 
 } // end  open shutter process -------------------------------------------------------------------------------------
 
 void close_shutter() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
-  Serial.println("Shutter close Process");          // testing only
+  //Serial.println("Shutter close Process");          // testing only
 
   stepper.moveTo(closeposition);
 
 
-  Serial.print("distance to go function value " );
-  Serial.println(stepper.distanceToGo());
+  //Serial.print("distance to go function value " );
+  //Serial.println(stepper.distanceToGo());
 
 
 
@@ -229,66 +208,7 @@ void close_shutter() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   {
     last_state = "closed";
   }
-  Serial.print("Steper current position is ");
-  Serial.println(stepper.currentPosition());
+  //Serial.print("Steper current position is ");
+  //Serial.println(stepper.currentPosition());
 
 } // end close shutter process -------------------------------------------------------------------------------------- -
-
-
-//--------------------- below not used yet - coded in readiness for the lower flap motor ------------------------------------
-
-
-void flap_close_process() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-{
-  //close the flap
-
-  initialise_relays();  // TURN THE POWER OFF
-
-
-  digitalWrite(FLAPRELAY1, HIGH);          // retracting POLARITY - Flap CLOSES first - the way the mechanics works is that the
-  digitalWrite(FLAPRELAY2, LOW);           // linear actuator has to retract to close the flap
-
-  // Serial.println("waiting for the flap switch to close ...");
-
-  while (digitalRead(Flapclosed) == HIGH)      //keep reading the Hall sensor
-  {
-
-    digitalRead(Flapclosed);                  // this is a redundant statement
-
-  }   // endwhile flapclosed
-
-
-  initialise_relays();  // TURN THE POWER OFF
-
-} // end void flap_close_process() ------------------------------------------------------------------------------------
-
-
-void flap_open_process() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-
-{
-  // open the flap
-  initialise_relays();
-
-  digitalWrite(FLAPRELAY1, LOW);
-  digitalWrite(FLAPRELAY2, HIGH);
-
-
-  while (digitalRead(Flapopen) == HIGH)         // keep reading the hall sensor until it changes (i.e. flap is fully open)
-  {
-    // debug on the open brace - {digitalRead(FLAPRELAY1)}{digitalRead(FLAPRELAY2)}
-
-    digitalRead(Flapopen);                      // this is a redundant statement
-
-  }
-
-  initialise_relays();  // TURN THE POWER OFF
-
-} // end void flap_open_process()  ------------------------------------------------------------------------------------
-
-void initialise_relays() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-{
-  //  Serial.println( "  Initialising relays ");
-  digitalWrite(FLAPRELAY1,    HIGH) ;
-  digitalWrite(FLAPRELAY2,    HIGH) ;
-
-} // end intialise relays ---------------------------------------------------------------------------------------------------
---
+++ src/Radio_Shutter.h
@@ -0,0 +1,10 @@
+#pragma once
+#include <Arduino.h>
+#include <SoftwareSerial.h>
+#include <AccelStepper.h>
+
+void open_shutter();
+void close_shutter();
+void flap_close_process();
+void flap_open_process();
+void initialise_relays() ;
