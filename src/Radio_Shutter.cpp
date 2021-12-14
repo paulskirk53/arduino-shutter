07b8e42652b00ad28c76c465f17291d3ddac374f
Paul <paulskirk53@gmail.com>
2021-12-14 22:44:25 +0000

    some bugs in the newly introduced shutter closed sensor code. Digitalread was missed out

--- src/Radio_Shutter.cpp
+++ src/Radio_Shutter.cpp
@@ -1,5 +1,7 @@
 
 
+//December '21 wdt added BUT the shutter takes about 30 seconds to close, with the stepper.run in a while loop with no no dogkick
+//so why hasn't the wdt rest the system? Obviously we don't want a reset, so once the problem has been sorted, dogkick will need to be in the loops
 // July 9th '21 started removing the flap based code as this is not required for the pulsar dome
 // this file was a clone done on 9th July from origin so is a working version for the new Pulsar dome.
 // 22-7-21 adding a new function to switch on and off the DC (battery) power to the Shutter stepper - tested and works
@@ -18,26 +20,27 @@
 
 // power management for shutter stepper controller - MA860H - this is via the SS relay
 // and the power for the rain sensor device - this only draws 8 mA so can easily be powered from a pin
-#define power_pin 9                     // this drives the gate of the SSR40DD device to turn on the power to the motor which opens / closes the shutter added the pin on 22-7-21
+#define power_pin    9                  // this drives the gate of the SSR40DD device to turn on the power to the motor which opens / closes the shutter added the pin on 22-7-21
 #define SensorPower 10                  // rain sesnor power pin added 30-7-21
 
 // step, dir and enable pin definitions
 #define stepPin 7                       // step pin tested and works - motor moves
-#define dirPin 8
+#define dirPin  8
 
 // create the stepper instance
 AccelStepper stepper(AccelStepper::DRIVER, stepPin, dirPin, true);
 
 // Data pin definitions
 
-#define open_shutter_command 36      // input pin
-#define close_shutter_command 47     // input pin
-#define emergency_stop 30            // input pin   green (mcu side ) blue on HC side
-#define shutter_status 48            // OUTPUT pin
-#define push_button_open_shutter 52  // mauve (MCU side) to green (HC cable side) to hand control switch unit
-#define push_button_close_shutter 53 // blue (MCU side) to yellow (HC cable side) hand control switch unit
-#define Rain_Monitor 11              //  rain monitor data pin
-#define MCU_reset    13              // this is pulled LOW by the command processor to reset this cpu
+#define open_shutter_command      36   // input pin
+#define close_shutter_command     47   // input pin
+#define emergency_stop            30   // input pin   green (mcu side ) blue on HC side
+#define shutter_status            48   // OUTPUT pin
+#define push_button_open_shutter  52   // mauve (MCU side) to green (HC cable side) to hand control switch unit
+#define push_button_close_shutter 53   // blue (MCU side) to yellow (HC cable side) hand control switch unit
+#define Rain_Monitor              11   // rain monitor data pin
+#define closedSensor_pin          12   // looks like it's free - Pin goes LOW when magnet is detected
+#define MCU_reset                 13   // this is pulled LOW by the command processor to reset this cpu
 #define on true
 #define off false
 
@@ -67,10 +70,10 @@ void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   pinMode (MCU_reset, INPUT_PULLUP);
   pinMode(open_shutter_command, INPUT_PULLUP);
   pinMode(close_shutter_command, INPUT_PULLUP);
-  pinMode(emergency_stop, INPUT_PULLUP); // user push button to stop the motor
-  pinMode(shutter_status, OUTPUT);       // this routine sets this pin and it is read by the command processor arduino
-
-  pinMode(push_button_open_shutter, INPUT); // changed from INPUT_PULLUP
+  pinMode(emergency_stop, INPUT_PULLUP);        // user push button to stop the motor
+  pinMode(shutter_status, OUTPUT);              // this routine sets this pin and it is read by the command processor arduino
+  pinMode(closedSensor_pin, INPUT_PULLUP );            // this wilL BE high WHEN THE SHUTTER is open
+  pinMode(push_button_open_shutter, INPUT);     // changed from INPUT_PULLUP
   pinMode(push_button_close_shutter, INPUT_PULLUP);
 
   digitalWrite(shutter_status, HIGH); // HIGH means closed
@@ -84,6 +87,7 @@ void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   // Power should only be on when testing for rain to avoid corrosion of the sensor pad.
 
   // stepper setup:
+  closeposition    = 0;
   StepsPerSecond = 500.0;              // changed following empirical testing
   normalAcceleration = 50;             // changed following empirical testing
   stepper.setMaxSpeed(StepsPerSecond); // steps per second see below -
@@ -91,15 +95,32 @@ void setup() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
   stepper.setAcceleration(normalAcceleration);
   stepper.setCurrentPosition(closeposition); // initial position for stepper is closed
 
-  last_state       = "closed";
-  openposition     = 4000; // set to a number defined empirically
-  closeposition    = 0;
+  last_state       = "closed";    //this is an assumption which may not be true, especially if wdt resets
+  openposition     = 3300; // was set to 4000 which was a bit too high. Changed December 2021
+  
   rainSensorEnable = true;
   powerOnDuration  = 30000; // 30 seconds
   powerOnFlag      = off;
   // delay below introduced to give the command processor time to define its pin states, which are used by this program
 
   delay(5000);
+
+/* write some code here which checks if the hall sesnor at the closed position is detecting 'closed'
+   if not, invoke a shutter initialisation routine which calls closeshutter but stops the movement when the Hall sensor detects closed
+
+*/
+ if ( digitalRead(closedSensor_pin) == HIGH )   // pin is HIGH if the shutter is OPEN
+  {    // write code here to close the shutter
+       // assume the shutter is at worst case fully open when a reset occurs
+       // it might be best to go at half the normal speed or less, so as not to shock the motor / chain when the hall sensor is reached
+
+    stepper.setCurrentPosition(openposition);  // assume worst case of shutter fully open
+    PowerOn();
+    close_shutter();    // note an amendment to the while loop condition in close shutter needs to be checked for Hall sensor logic - is closed HIGH or LOW?
+    PowerOff();
+
+  }
+
 
   wdt_enable(WDTO_4S);                 //Watchdog set to 4 seconds note this statement is placed after the delay
   // Note there is a 2 second delay in the poweron() routine, so 4 seconds seems reasonable for the wdt.
@@ -176,10 +197,9 @@ void loop() // +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 // end void loop ----------------------------------------------------------------------------------------------------------
 void dogkick()
 {
-    if ( digitalRead(MCU_reset == HIGH)  )
-    {
+        
      wdt_reset();                       //execute this command within 4 seconds to keep the timer from resetting the CPU
-    }
+    
 }
 void open_shutter() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 {
@@ -212,7 +232,7 @@ void close_shutter() // ++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
   stepper.moveTo(closeposition);
 
-  while ((stepper.distanceToGo() != 0) && (digitalRead(emergency_stop) == LOW))
+  while ((stepper.distanceToGo() != 0) && (digitalRead(emergency_stop) == LOW)  && ( digitalRead(closedSensor_pin) == HIGH)   )  // Pin is HIGH while the shutter is OPEN
   {
     stepper.run();
   }
